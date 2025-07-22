2addec6b6cf069210560b4e441a218d56cef7bf8
Kings-bop <hothaisonnn@gmail.com>
2025-07-22 15:13:41 +0700

    da sua ok

--- trang_chu_admin/update_theater.php
+++ trang_chu_admin/update_theater.php
@@ -2,14 +2,14 @@
 header('Content-Type: application/json');
 require_once 'connect.php';
 
-$data = json_decode(file_get_contents('php://input'), true);
-$id = (int)$data['id'];
-$address = $conn->real_escape_string($data['address']);
-$overview = $conn->real_escape_string($data['overview']);
+$data = json_decode(file_get_contents("php://input"), true);
+$id = isset($data['id']) ? (int)$data['id'] : 0;
+$address = $data['address'];
+$overview = $data['overview'];
 
 $sql = "UPDATE khu_tro SET dia_chi = ?, tong_quan = ?, ngay_cap_nhat = CURRENT_TIMESTAMP WHERE id_khu_tro = ?";
 $stmt = $conn->prepare($sql);
-$stmt->bind_param("ssi", $address, $overview, $id);
+$stmt-> $conn->prepare("ssi", $address, $overview, $id);
 
 if ($stmt->execute()) {
     echo json_encode(['success' => true]);
